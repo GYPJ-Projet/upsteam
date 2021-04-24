83ddee9d9c9fd717f919a221417a424fd566df85
Yulia Merkulova <yulia.merkulova.ym@gmail.com>
2021-04-23 23:25:32 -0400

    Gestion marques CSS

--- vues/listeDonnees.php
+++ vues/listeDonnees.php
@@ -1,10 +1,11 @@
 <?php   
 ?>
-<div class="listeDonnees">
+<div class="bodyConteneur">
+<aside class="listeDonnees">
     <ul>
         <li><a href="index.php?GestionDonnees&action=gestionMarque">Marque</a></li>
         <li><a href="index.php?GestionDonnees&action=gestionModele">Modele</a></li>
         <li><a href="index.php?GestionDonnees&action=gestionCouleur">Couleur</a></li>
         <li><a href="index.php?GestionDonnees&action=gestionVoiture">Voiture</a></li>
     </ul>
-</div>
+</aside>
--- vues/pageDonnees.php
+++ vues/pageDonnees.php
@@ -1,6 +1,7 @@
 <section class="pageDonnees">
-    <h1>Gestion des données</h1>
+    <h1>Gestion des données</h1><br/>
     <p>Cette page est utilisée pour saisir des données dans la base de données.</p>
     <p><<-- À gauche, sélectionnez la section avec laquelle vous souhaitez travailler.</p>
     <p>Bon travail!</p>
 </section>
+</div>
