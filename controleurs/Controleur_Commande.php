6ccecc2f4ce7a6f6908b8b73e888062698aa8079
Jean Senneville <sennevij@gmail.com>
2021-05-04 13:36:12 -0400

    creation du controleur_Commande et fichier de langue correspondant (base seulement)

---
+++ controleurs/Controleur_Commande.php
@@ -0,0 +1,55 @@
+<?php
+	class Controleur_Commande extends BaseControleur {
+
+		// Méthode qui retourne le nom du contrôleur où aller chercher la langue 
+		public function getNomControleur() {
+			return "Commande";
+		}
+
+		// La fonction qui sera appelée par le routeur
+		public function traite(array $params) {
+			
+			// Initialisation des donnees a un tableau vide par défaut
+			$donnees = array();
+			
+			// On charge les fichiers de langue selon la langue choisi par l'usager.
+			$donnees["langue"] = $this->chargerLangue($params);
+
+			$idLangue = $donnees["langue"]["idLangue"]; // On récupère l'ID de la langue
+
+			$this->afficheVue("tete");
+			$this->afficheVue("entete", $donnees);
+            $this->afficheVue("menu", $donnees);
+
+
+            // Si on a reçu une action, on la traite...
+			if (isset($params["action"])) {
+
+				// Switch en fonction de l'action qui est envoyée en paramètre de la requête
+				// Ce switch détermine la vue $vue et obtient le modèle $data
+				switch($params["action"]) {
+
+					case "afficerCommande" :
+
+						// Affichage de la commande du client 
+						// Si on a reçu les paramètres Panier .
+						if (isset($params["panier"])) {
+
+							 // on converti le tableau reçu en array
+							 $tabPanier = json_decode($params["panier"], true);
+						}
+
+						$this->afficheVue("afficerCommande", $donnees);
+
+						break;
+
+                   
+				}			
+			} else {
+				// traitement à determiner ici
+			}
+
+			$this->afficheVue("piedDePage", $donnees);
+		}
+	}
+?>
