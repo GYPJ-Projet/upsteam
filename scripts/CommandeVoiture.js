b06b8bdbc983dc45ce95646af150002f47e63c40
Gloria Kiese <glojoys@hotmail.com>
2021-05-12 11:12:28 -0400

    page commande modifiée une seconde fois

--- scripts/CommandeVoiture.js
+++ scripts/CommandeVoiture.js
@@ -5,15 +5,13 @@ class CommandeVoiture {
         this._elRetrait = this._el.querySelector('[data-js-retrait]');
         this._elQteVoiture = this._el.querySelector('[data-js-quantite]');
         this._elPrix = this._el.querySelector('[data-js-prix]');
-        //this._elMontant = this._el.querySelector('[data-js-montant]');
         this._elTotalPartiel = document.querySelector('[data-js-total-partiel]'); 
         this._panier = JSON.parse(localStorage.getItem('panierAchat'));
         this._nbrVoiture = document.querySelector('[data-js-nombre-voiture]');   
         this._idVoiture = parseInt(this._el.dataset.jsCommandevoiture);
         this._elControleurAction = document.querySelector('[data-js-controleur-action]');  
         this._Commander = document.querySelector('[data-js-commander]'); 
-            
-        
+                
         Taxes.getTaxes();
 
         this.init();
@@ -30,29 +28,32 @@ class CommandeVoiture {
    
     retirerVoiture = (e) => {        
         let qte = parseInt(this._elQteVoiture.innerHTML);
-        let montant =  parseInt(this._elMontant.innerHTML);
         let prix =  parseInt(this._elPrix.innerHTML);
         let sousTotal = parseInt(this._elTotalPartiel.innerHTML);
         let nbrVoiture = parseInt(localStorage.getItem('nombreVoiture')); 
 
- 
         if (qte > 0) { 
             qte--;  
-            this._panier[this._idVoiture].quantite--; 
+            this._panier[this._idVoiture] = null;
             nbrVoiture--;
+            console.log(nbrVoiture);
+
             localStorage.setItem('panierAchat', JSON.stringify(this._panier));      
-            localStorage.setItem('nombreVoiture', JSON.stringify(this._nbrVoiture));      
+            localStorage.setItem('nombreVoiture', JSON.stringify(nbrVoiture));      
             this._elControleurAction.dataset.jsControleurAction = `afficherCommande&panier= ${JSON.stringify(this._panier)}`;   
            
-            montant = prix * qte;
             sousTotal -= prix;
+            console.log(this._el.innerHTML);
+            console.log(this._el.innerHTML);
 
             this._nbrVoiture.innerHTML = nbrVoiture;
             this._elQteVoiture.innerHTML = qte;
-            //this._elMontant.innerHTML = montant;
             this._elTotalPartiel.innerHTML = sousTotal;
-            this._el.setAttribute('style', 'display:none');
-            
+            this._el.innerHTML = '';
+
+            console.log(this._el.innerHTML);
+
+        
             if (this._panier == null) {
                 if (this._nbrVoiture == 0){
                     this._Commander.classList.add('hidden')
